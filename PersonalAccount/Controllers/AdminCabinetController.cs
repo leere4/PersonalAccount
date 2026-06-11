3078835856f814bb7d74d37581108220773e8972
leere4 <leere161@gmail.com>
2026-06-12 00:35:28 +0400

    Выполнено ДЗ 5: Роли, кабинет администратора, подтверждение почты

--- PersonalAccount/Controllers/AdminCabinetController.cs
+++ PersonalAccount/Controllers/AdminCabinetController.cs
@@ -1,34 +1,60 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PersonalAccount.Services.Cabinet;
+using PersonalAccount.Services.Tokens;
 using PersonalAccount.Types;
 using PersonalAccount.ViewModels;
 
 namespace PersonalAccount.Controllers;
 
 [Authorize(Roles = AccountRoleConstants.Admin)]
-public class AdminCabinetController(IAdminCabinetService cabinetService) : Controller
+public class AdminCabinetController(
+	IAdminCabinetService cabinetService,
+	IConfirmationTokenService confirmationTokenService) : Controller
 {
-    [HttpGet]
-    public async Task<IActionResult> Index()
-    {
-        var accounts = (await cabinetService.GetAllStudentAccountsAsync())
-            .ToDictionary(account => account.Id);
-        var groups = (await cabinetService.GetAllGroupsAsync())
-            .ToDictionary(group => group.Id);
-        var studentProfiles = await cabinetService.GetAllStudentProfilesAsync();
+	[HttpGet]
+	public async Task<IActionResult> Index()
+	{
+		var accountsList = await cabinetService.GetAllStudentAccountsAsync();
+		var studentProfiles = await cabinetService.GetAllStudentProfilesAsync();
+		var groupsList = await cabinetService.GetAllGroupsAsync();
 
-        return View(new AdminCabinetViewModel
-        {
-            Students = studentProfiles.Select(studentProfile => new AdminCabinetStudentViewModel
-            {
-                FullName = studentProfile.FullName,
-                GroupName = studentProfile.GroupId == null
-                    ? "Без группы"
-                    : groups[studentProfile.GroupId.Value].Name,
-                PhotoUrl = studentProfile.PhotoUrl?.ToString(),
-                Email = accounts[studentProfile.AccountId].Email
-            }).ToList()
-        });
-    }
+		var accounts = accountsList.ToDictionary(acc => acc.Id);
+		var groups = groupsList.ToDictionary(g => g.Id);
+
+		var studentsViewModel = new List<AdminCabinetStudentViewModel>();
+
+		foreach (var studentProfile in studentProfiles)
+		{
+			if (!accounts.TryGetValue(studentProfile.AccountId, out var account)) continue;
+
+			var groupName = "Без группы";
+			if (studentProfile.GroupId != null && groups.ContainsKey(studentProfile.GroupId.Value))
+			{
+				groupName = groups[studentProfile.GroupId.Value].Name;
+			}
+
+			var isConfirmed = await confirmationTokenService.HasConfirmedTokenAsync(studentProfile.AccountId);
+
+			studentsViewModel.Add(new AdminCabinetStudentViewModel
+			{
+				AccountId = studentProfile.AccountId,
+				FullName = studentProfile.FullName,
+				GroupName = groupName,
+				PhotoUrl = studentProfile.PhotoUrl?.ToString(),
+				Email = account.Email,
+				IsEmailConfirmed = isConfirmed
+			});
+		}
+
+		return View(new AdminCabinetViewModel { Students = studentsViewModel });
+	}
+
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> ConfirmStudentEmail(int id)
+	{
+		await cabinetService.ConfirmStudentEmailAsync(id);
+		return RedirectToAction("Index");
+	}
 }
--- PersonalAccount/Services/Cabinet/AdminCabinetService.cs
+++ PersonalAccount/Services/Cabinet/AdminCabinetService.cs
@@ -1,20 +1,29 @@
 ﻿using PersonalAccount.Models;
 using PersonalAccount.Repositories;
+using PersonalAccount.Services.Tokens; 
 using PersonalAccount.Types;
 
 namespace PersonalAccount.Services.Cabinet;
 
 public class AdminCabinetService(
-    IAccountRepo accountRepo,
-    IStudentProfileRepo studentProfileRepo,
-    IGroupRepo groupRepo
+	IAccountRepo accountRepo,
+	IStudentProfileRepo studentProfileRepo,
+	IGroupRepo groupRepo,
+	IConfirmationTokenService confirmationTokenService 
 ) : IAdminCabinetService
 {
-    public async Task<List<AccountModel>> GetAllStudentAccountsAsync() =>
-        await accountRepo.GetAllByRole(AccountRoles.Student);
+	public async Task<List<AccountModel>> GetAllStudentAccountsAsync() =>
+		await accountRepo.GetAllByRole(AccountRoles.Student);
 
-    public async Task<List<StudentProfileModel>> GetAllStudentProfilesAsync() => 
-        await studentProfileRepo.GetAllAsync();
+	public async Task<List<StudentProfileModel>> GetAllStudentProfilesAsync() =>
+		await studentProfileRepo.GetAllAsync();
 
-    public async Task<List<GroupModel>> GetAllGroupsAsync() => await groupRepo.GetAllAsync();
+	public async Task<List<GroupModel>> GetAllGroupsAsync() => await groupRepo.GetAllAsync();
+
+	
+	public async Task ConfirmStudentEmailAsync(int accountId)
+	{
+		var token = await confirmationTokenService.GenerateTokenAsync(accountId);
+		await confirmationTokenService.ValidateTokenAsync(accountId, token);
+	}
 }
